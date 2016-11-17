3cd1b249ad5ea401d281fc50e97781ecdbe2da4f
Nicholas <snickdx@gmail.com>
2016-11-16 23:37:43 -0400

    putting back the count text

--- gulpfile.js
+++ gulpfile.js
@@ -49,3 +49,14 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
+
+gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+  var rootDir = 'www';
+  
+  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    stripPrefix: rootDir
+  }, callback);
+});
